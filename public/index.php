e4ca26b49c3ac0a44e807611acf61e9956596d61
Rungen Zhu <cloverzrg@gmail.com>
2017-11-17 16:44:16 +0800

    vendor 目录常量

--- public/index.php
+++ public/index.php
@@ -14,7 +14,9 @@ define('BASE_PATH', dirname(__DIR__) . DS);
 define("CORE", BASE_PATH . 'core' . DS);
 define('CONFIG_PATH', BASE_PATH . 'config' . DS);
 define("APP", BASE_PATH . 'app' . DS);
-define("FRAMEWORK_NAME","jeongen.com");
+define("FRAMEWORK_NAME", "jeongen.com");
+define("VENDOR_PATH", BASE_PATH . 'vendor' . DS);
+
 
 // 加载框架函数库
 require_once CORE . 'function.php';
@@ -26,7 +28,7 @@ require_once APP . 'function.php';
 require_once CORE . 'Main.php';
 
 //composer
-require_once BASE_PATH.'vendor'.DS.'autoload.php';
+require_once VENDOR_PATH . 'autoload.php';
 
 // 注册自动加载类
 spl_autoload_register('\core\Main::loader');
